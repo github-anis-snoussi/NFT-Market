61ba82f91c4cbbf1d53d9ddaff9cf23f52af3e1c
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-11-11 00:12:26 +0100

    deployed on localhost

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+/*
+  to test locally;
+    run a local node: npx hardhat node
+    deploy the contract: npx hardhat run scripts/deploy.js --network localhost
+*/
+
+async function main() {
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy();
+  await nftMarket.deployed();
+  console.log("nftMarket deployed to:", nftMarket.address);
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+  console.log("nft deployed to:", nft.address);
+
+  // let config = `
+  // export const nftmarketaddress = "${nftMarket.address}"
+  // export const nftaddress = "${nft.address}"
+  // `;
+
+  // let data = JSON.stringify(config);
+  // fs.writeFileSync("config.js", JSON.parse(data));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
