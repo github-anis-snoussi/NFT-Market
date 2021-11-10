a968c0fe4ee1052575c1be95349bbd6bd5543624
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-11-11 00:33:47 +0100

    can create NFTs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -18,13 +18,13 @@ async function main() {
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
 
-  // let config = `
-  // export const nftmarketaddress = "${nftMarket.address}"
-  // export const nftaddress = "${nft.address}"
-  // `;
+  let config = `
+  export const nftmarketaddress = "${nftMarket.address}"
+  export const nftaddress = "${nft.address}"
+  `;
 
-  // let data = JSON.stringify(config);
-  // fs.writeFileSync("config.js", JSON.parse(data));
+  let data = JSON.stringify(config);
+  fs.writeFileSync("config.js", JSON.parse(data));
 }
 
 main()
